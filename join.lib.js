6c24b3532a0f87e5ec0e1cc96c0627be35874e83
Giang Hoang Nguyen <nhgiang94@gmail.com>
2018-10-16 06:04:10 +0700

    Submit assignment

--- join.lib.js
+++ join.lib.js
@@ -231,6 +231,7 @@ JoinForm.prototype.clearError = function ( fieldName ) {
     if(fieldName){
     $s(fieldName + "_error").className = "";
     $s(fieldName + "_error").firstChild.nodeValue = "";
+    $s(fieldName + "_error").removeAttribute("role");
     }
 }
 
@@ -249,9 +250,9 @@ JoinForm.prototype.validateForm = function () {
             // Uncomment the following if statement to add an ARIA alert to the error message
             // Only the last alert is read, so limit alerts to the first error
             // so it matches with focus sent to the first message
-            //if(error_count == 1){
-            //	$s(fieldName + "_error").setAttribute("role", "alert");
-            //}
+            if(error_count == 1){
+            	$s(fieldName + "_error").setAttribute("role", "alert");
+            }
             $s(fieldName + "_error").firstChild.nodeValue = error.message;
             if(error_count == 1){
             	$s(fieldName).focus();
@@ -265,7 +266,7 @@ JoinForm.prototype.validateForm = function () {
         $s("feedback").firstChild.nodeValue = this.success;
         $s("feedback").className = "feedback";
        // Uncomment the next line to add an ARIA alert to the feedback message
-       // $s("feedback").setAttribute("role", "alert");
+        $s("feedback").setAttribute("role", "alert");
     }
     return hasErrors;
 }
