f3d362da66fffd7469fd72b9fa6c13af4f165519
Giang Hoang Nguyen <nhgiang94@gmail.com>
2018-10-20 12:44:59 +0700

    Access progress bar

--- assets/ik_progressbar.js
+++ assets/ik_progressbar.js
@@ -2,6 +2,7 @@
 	
 	var pluginName = 'ik_progressbar',
 		defaults = { // values can be overitten by passing configuration options to plugin constructor 
+'instructions': 'Press spacebar, or Enter to get progress',
 			'max': 100
 		};
 	
@@ -31,16 +32,35 @@
 		this.element
 			.attr({
 				'id': id,
+'tabindex': -1,
+'role': 'progressbar',
+'aria-valuenow': 0, // set current value to 0
+'aria-valuemin': 0, // set minimum (start) value to 0 (required by screen readers)
+'aria-valuemax': this.options.max, // set maximum (end) value
+'aria-describedby': id + '_instructions'
 			})
 			.addClass('ik_progressbar')
-      ;
+.on('keydown', {'plugin': this}, this.onKeyDown);
 		
 		this.fill = $('<div/>')
 			.addClass('ik_fill');
 			
 		this.notification = $('<div/>') // add div element to be used to notify about the status of download
+.attr({
+'aria-live': 'assertive', // set notification priority to high
+'aria-atomic': 'additions'
+})
 			.addClass('ik_readersonly')
 			.appendTo(this.element);
+
+$('<div/>') // add div element to be used with aria-described attribute of the progressbar
+.text(this.options.instructions) // get instruction text from plugin options
+.addClass('ik_readersonly') // hide element from visual display
+.attr({
+'id': id + '_instructions',
+'aria-hidden': true
+})
+.appendTo(this.element);
 
 		$('<div/>')
 			.addClass('ik_track')
@@ -58,7 +78,7 @@
 		
 		var value;
 		
-		value = Number( this.element.data('value') ); // inaccessible
+value = Number(this.element.attr('aria-valuenow')); // accessible
 		
 		return parseInt( value );
 		
@@ -98,11 +118,7 @@
 			val = n;
 		}
 		
-		this.element
-			.data({ // inaccessible
-				'value': parseInt(val) 
-			}) 
-      ;
+		this.element.attr({'aria-valuenow': val});
 		
 		this.updateDisplay();
 		
@@ -132,6 +148,24 @@
 		this.notify();
 	
 	};
+	
+/** 
+* Handles kedown event on progressbar element. 
+*
+* @param {Object} event - Keyboard event.
+* @param {object} event.data - Event data.
+* @param {object} event.data.plugin - Reference to plugin.
+*/
+Plugin.prototype.onKeyDown = function(event) {
+switch(event.keyCode) {
+case ik_utils.keys.space:
+case ik_utils.keys.enter:
+event.preventDefault();
+event.stopPropagation();
+event.data.plugin.notify();
+break;
+}
+};
 	
 	$.fn[pluginName] = function ( options ) {
 		
